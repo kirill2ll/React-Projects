c8022d4b9bf921eff06bddfd8090d4458617b53b
kirill2ll <kirill2ll@mail.ru>
2023-09-29 11:09:44 +0300

    Add platform display

---
+++ GamesPortal/GamesPortal/src/components/PlatformiconList.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformiconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    web: BsGlobe,
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+    nintendo: SiNintendo,
+    pc: FaWindows,
+    xbox: FaXbox,
+    playstation: FaPlaystation,
+    mac: FaApple,
+    linux: FaLinux,
+  };
+
+  return (
+    <HStack marginY={"10px"}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformiconList;
