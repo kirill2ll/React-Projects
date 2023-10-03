03096a7c91988ec2f336e6e0ab73c1fa546b7cd8
kirill2ll <kirill2ll@mail.ru>
2023-10-03 21:38:16 +0300

    Add Filtering functionality by platform

--- GamesPortal/GamesPortal/src/App.tsx
+++ GamesPortal/GamesPortal/src/App.tsx
@@ -5,9 +5,13 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   return (
     <>
@@ -27,11 +31,16 @@ function App() {
               selectedGenre={selectedGenre}
               onSelectGenre={setSelectedGenre}
             />
-          </GridItem> 
+          </GridItem>
         </Show>
         <GridItem area="main">
-          <PlatformSelector></PlatformSelector>
-          <GameGrid selectedGenre={selectedGenre}></GameGrid>
+          <PlatformSelector selectedPlatform = {selectedPlatform}
+            onSelectPlatform={setSelectedPlatform}
+          ></PlatformSelector>
+          <GameGrid
+            selectedPlatform={selectedPlatform}
+            selectedGenre={selectedGenre}
+          ></GameGrid>
         </GridItem>
       </Grid>
     </>
