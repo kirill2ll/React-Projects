7f3002f1434992148bec605fdd0ea921cfb47a45
kirill2ll <kirill.kvitkovski@gmail.com>
2025-04-29 19:12:16 +0300

    [Essentials] add CoreConcept component and display it

--- Essentials/src/App.jsx
+++ Essentials/src/App.jsx
@@ -1,9 +1,20 @@
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png'
+import { CORE_CONCEPTS } from './data.js';
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <h2>Core Concepts</h2>
+        <section id="core-concepts">
+          <ul>
+            {CORE_CONCEPTS.map((concept, index) => {
+             return <CoreConcept key={index} {...concept} />
+            })}
+          </ ul>
+        </section>
       </main>
     </div>
   );
@@ -20,13 +31,23 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>);
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
 }
 
 export default App;
