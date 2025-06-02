00a17eb6c71d62c7f808604a4a0f2a81712b6fec
kirill2ll <kirill.kvitkovski@gmail.com>
2025-06-02 13:19:19 +0300

    [ProjectManagement] add Project

--- ProjectManagement/src/App.jsx
+++ ProjectManagement/src/App.jsx
@@ -1,22 +1,32 @@
+import Project from "./components/Project";
 import Sidebar from "./components/Sidebar";
 import { useState, useRef } from "react";
 
 
 function App() {
   const [projects, setProjects] = useState([]);
-  const inputProject = useRef();
+  const [displayNewProject, setDisplayNewProject] = useState(false);
+  
 
 
-  function handleAddProject(name){
-    setProjects([...projects, {name: name, tasks: []}])
+  function handleAddProject(isVisible){
+    setDisplayNewProject(isVisible);
+  }
+
+  function saveNewProject(title, description, dueDate){
+    setProjects([...projects, {name: title, description, dueDate}]);
+    setDisplayNewProject(false);
   }
 
   return (
     <>
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-      <Sidebar title="Projects" projects={projects} ref={inputProject} handleAddProject={handleAddProject}/>
+      <Sidebar title="Projects" projects={projects} handleAddProject={handleAddProject}/>
+
+      {displayNewProject && <Project saveNewProject={saveNewProject} handleAddProject={handleAddProject}/>}
     </>
   );
 }
+
 
 export default App;
--- ProjectManagement/src/components/Sidebar.jsx
+++ ProjectManagement/src/components/Sidebar.jsx
@@ -1,9 +1,8 @@
-export default function Sidebar({ title, projects, ref, handleAddProject }) {
+export default function Sidebar({ title, projects, handleAddProject }) {
     return (
         <section>
             <h1>{title}</h1>
-            <input type="text" ref={ref} />
-            <button onClick={() => handleAddProject(ref.current.value)}>Add Project</button>
+            <button onClick={() => handleAddProject(true)}>Add Project</button>
             <ul className="project-list">
                 {projects.map((project) => {
                     return <li key={project.name}>{project.name}</li>
