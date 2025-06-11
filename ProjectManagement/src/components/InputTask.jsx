69d8e32162f0616c02ee786d1bb6e04f60db8ef1
kirill2ll <kirill.kvitkovski@gmail.com>
2025-06-12 01:12:31 +0300

    [ProjectManagement] add validation to InputTask

--- ProjectManagement/src/components/InputTask.jsx
+++ ProjectManagement/src/components/InputTask.jsx
@@ -1,10 +1,30 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
+import Modal from "./Modal";
 
-export default function InputTask({handleAddTaskToProject}) {
-    const [enteredTask, setEnteredTask] =  useState("");
+export default function InputTask({ handleAddTaskToProject }) {
+    const [enteredTask, setEnteredTask] = useState("");
+    const modal = useRef(); 
 
-    return <div className="flex items-center gap-4">
-        <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" value={enteredTask} onChange={(e) => setEnteredTask(e.target.value)} />
-        <button className="text-stone-700 hover:text-stone-900" onClick={() => handleAddTaskToProject(enteredTask)}>Add Task</button>
-    </div>
+    function AddTask() {
+        const task = enteredTask.trim();
+        if (task === "") {
+            modal.current.showModal();
+            return;
+        }
+        handleAddTaskToProject(task);
+        setEnteredTask("");
+    }
+
+
+    return (
+    <>
+        <Modal ref={modal}>
+            <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid input</h2>
+            <p className='text-stone-600 mb-4'>The task cannot be empty</p>
+        </Modal>
+        <div className="flex items-center gap-4">
+            <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" value={enteredTask} onChange={(e) => setEnteredTask(e.target.value)} />
+            <button className="text-stone-700 hover:text-stone-900" onClick={AddTask}>Add Task</button>
+        </div>
+    </>)
 } 
