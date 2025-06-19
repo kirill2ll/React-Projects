2cf2878ba8dc873ecc720855ef2b64a1bfffcf68
kirill2ll <kirill.kvitkovski@gmail.com>
2025-06-19 20:45:23 +0300

    [SideEffect][PlacePicker] update modal to use useEffect

--- PlacePicker/src/App.jsx
+++ PlacePicker/src/App.jsx
@@ -12,10 +12,10 @@ const storedIds = JSON.parse(localStorage.getItem('picked-places')) || [];
 const storedPlaces = storedIds.map((id) => AVAILABLE_PLACES.find((place) => place.id === id));
 
 function App() {
-  const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -26,12 +26,12 @@ function App() {
 
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -52,7 +52,7 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false);
 
     const storedIds = JSON.parse(localStorage.getItem('picked-places')) || [];
     localStorage.setItem('picked-places', JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)));
@@ -61,7 +61,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal isOpen={modalIsOpen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
--- PlacePicker/src/components/Modal.jsx
+++ PlacePicker/src/components/Modal.jsx
@@ -1,19 +1,16 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+function Modal({ isOpen, children }) {
   const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+  useEffect(() => {
+    if (isOpen) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [isOpen]);
 
   return createPortal(
     <dialog className="modal" ref={dialog}>
@@ -21,6 +18,6 @@ const Modal = forwardRef(function Modal({ children }, ref) {
     </dialog>,
     document.getElementById('modal')
   );
-});
+}
 
 export default Modal;
