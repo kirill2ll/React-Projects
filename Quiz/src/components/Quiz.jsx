0075f5f761b1d5294f8852bdb5da45c4d07b5fbd
kirill2ll <kirill.kvitkovski@gmail.com>
2025-06-28 18:13:39 +0300

    [Quiz] Quiz: add shuffle and complete logic

--- Quiz/src/components/Quiz.jsx
+++ Quiz/src/components/Quiz.jsx
@@ -1,20 +1,34 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
 
+import quizOver from "../assets/quiz-complete.png"
+
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
-    const activeQuestion = userAnswers.length;
+    const isOverQuiz = userAnswers.length === QUESTIONS.length;
 
     function handleSelectAnswer(answer) {
         setUserAnswers((prevAnswers) => [...prevAnswers, answer]);
     }
+
+    if (isOverQuiz) {
+        return (
+            <div id="summary">
+                <img src={quizOver} alt="Quiz Complete" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+    
+    const activeQuestion = userAnswers.length;
+    const shuffledAnswers = QUESTIONS[activeQuestion].answers.sort(() => Math.random() - 0.5);
 
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestion].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestion].answers.map(answer => {
+                    {shuffledAnswers.map(answer => {
                         return <li key={answer} className="answer"><button onClick={() => handleSelectAnswer(answer)}>{answer}</button></li>
                     })}
                 </ul>
